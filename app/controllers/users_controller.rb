f0a79d7116cafec656ca63a67080bbff75af8c3f
Samuel <lee.eunseong7856@gmail.com>
2023-01-13 10:14:07 -0600

    commit tree 4520d96aa7334fd08ed43b0c0d11bd02065b5b30

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,32 @@
 class UsersController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find_by!(id: params[:id])
     render json: user, include: :items
   end
 
+  def index_items
+    user_items = User.find_by!(id: params[:id]).items
+    render json: user_items, status: :ok
+  end
+
+  def create_user_items
+    user_create = User.find_by!(id: params[:user_id]).items.create!(item_params)
+    render json: user_create, status: :created
+  end
+
+  def show_item
+    item = User.find_by!(id: params[:user_id]).items.find_by!(id: params[:item_id])
+    render json: item, status: :ok
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found invalid
+    render json: { errors: 'Not Found' }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index, :create]
+
+  get '/users/:id/items', to: 'users#index_items'
+  get '/users/:user_id/items/:item_id', to: 'users#show_item'
+  post '/users/:user_id/items', to: 'users#create_user_items'
 end
